a4748ba4dc2c93dde5af137948acbfad81a24310
Alex <unlok@yandex-team.ru>
2014-08-14 17:01:02 +0400

    First release

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-gemini-coverage
-===============
+# Simple gemini coverage
 
-Simple gemini coverage
+[![Build Status](https://travis-ci.org/bem/gemini-coverage.svg?branch=master)](https://travis-ci.org/bem/gemini-coverage)
+
+###### Инструмент позволяет получать минимальную информацию о покрытии библиотеки **gemini**-тестами.
+
+### Установка
+
+```bash
+npm i gemini-coverage
+```
+### Использование
+
+```bash
+gemini-coverage <path_to_bem_library> # (стандартный, консольный отчет)
+gemini-coverage <path_to_bem_library> -r html # (отчет в виде html)
+gemini-coverage <path_to_bem_library> -r number # (отчет в виде чисел - 3/5)
+```
---
+++ test/scanner.js
@@ -0,0 +1,35 @@
+'uset strict';
+
+var Scanner = require('../lib/scanner');
+
+describe('Scanner', function() {
+
+        it('should reject with error if path is bad', function() {
+            return new Scanner('BAD_PATH')
+                .flow()
+                .should.be.rejectedWith(Error)
+        });
+
+        it('should return ceverage information by path', function() {
+            return new Scanner(__dirname + '/fixtures/bem-lib')
+                .flow()
+                .should.eventually.deep.equal(
+                    {
+                        covered: [
+                            {
+                                block: 'button',
+                                screens: 1,
+                                path: __dirname + '/fixtures/bem-lib/gemini/button.gemini.js'
+                            },
+                            {
+                                block: 'popup',
+                                screens: 2,
+                                path: __dirname + '/fixtures/bem-lib/gemini/popup.gemini.js'
+                            }
+                        ],
+                        notCovered: [ 'menu' ]
+                    }
+                );
+        });
+
+});
